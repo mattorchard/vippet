3065cb01d3f332a8e9c3dade8e90c6eb0825e91d
Matt Orchard <morch028@uottawa.ca>
2020-12-03 22:24:11 -0500

    Adds name and repo link to home page

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import {getVideoDetails} from "./helpers/mediaHelpers";
   import Editor from "./components/Editor.svelte";
   import WelcomeArea from "./components/WelcomeArea.svelte";
+  import {fade} from 'svelte/transition';
 
   let videoUrl: string | undefined;
   let isShowingTutorial = false;
@@ -39,20 +40,32 @@
     {/await}
 
 {:else}
-    <header>
-        <h1>Vippet</h1>
-        <h2>Video snippets, made easy.</h2>
-    </header>
-    <form>
-        <RecordInput onComplete={handleNewVideo} onIsIdleChange={isIdle => isShowingTutorial = isIdle}/>
-    </form>
-    {#if isShowingTutorial}
-        <WelcomeArea/>
-    {/if}
+    <div class="page-column">
+        <header>
+            <h1>Vippet</h1>
+            <h2>Video snippets, made easy.</h2>
+        </header>
+        <form>
+            <RecordInput onComplete={handleNewVideo} onIsIdleChange={isIdle => isShowingTutorial = isIdle}/>
+        </form>
+        {#if isShowingTutorial}
+            <WelcomeArea/>
+        {/if}
 
+        <footer in:fade={{duration: 2000, delay: 1500}}>
+            <a href="https://github.com/mattorchard/vippet">
+                Built with Svelte and TypeScript, by Matthew Orchard.
+            </a>
+        </footer>
+    </div>
 {/if}
 
 <style>
+  .page-column {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
   h1 {
     color: var(--color-accent);
     font-size: 4rem;
@@ -70,5 +83,15 @@
   form {
     padding: 3rem;
     text-align: center;
+  }
+
+  footer {
+    margin-top: auto;
+    padding: 1rem;
+    text-align: end;
+  }
+  footer > a {
+    color: var(--color-text-light);
+    text-decoration: underline;
   }
 </style>
